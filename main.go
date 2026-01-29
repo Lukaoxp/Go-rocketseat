56e936c80b3bee420528cfdc3b5295eff6a689d0
Lucas Carturani <lucascarturani@gmail.com>
2026-01-29 20:37:00 -0300

    estrutura de switch

--- main.go
+++ main.go
@@ -1,26 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"time"
 )
 
 func main() {
-	if err := retornarErro(); err != nil {
-		fmt.Println(err.Error())
+	today := time.Now().Weekday()
+
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("hoje")
+	case today + 1:
+		fmt.Println("amanha")
+	case today + 2:
+		fmt.Println("depois de amanha")
+	default:
+		fmt.Println("Ta longe")
 	}
-
-	players := map[string]int{
-		"lucas": 31,
-		"mario": 14,
-	}
-
-	if value, ok := players["lucas"]; ok {
-		fmt.Println("pontos: ", value, ok)
-	}
-
-}
-
-func retornarErro() error {
-	return errors.New("Isso é um erro")
 }
