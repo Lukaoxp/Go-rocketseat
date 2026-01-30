e134bf0b4b3b417057aadd87e9d9bd9ff132daea
Lucas Carturani <lucascarturani@gmail.com>
2026-01-29 22:19:56 -0300

    structs

--- main.go
+++ main.go
@@ -1,41 +1,30 @@
 package main
 
+import "fmt"
+
+type Cliente struct {
+	Nome     string
+	Idade    int
+	Endereco Endereco
+	Email    string
+}
+
+type Endereco struct {
+	Rua    string
+	Numero int
+	CEP    string
+}
+
 func main() {
-
-	// for normal
-	for i := 0; i < 10; i++ {
-		// println(i)
+	cliente := Cliente{
+		Nome:  "lucas",
+		Idade: 31,
+		Endereco: Endereco{
+			CEP:    "88000-000",
+			Numero: 100,
+		},
 	}
 
-	// ou
-	for i := range 10 {
-		println(i)
-	}
-
-	println()
-
-	// for como um while
-	sum := 1
-	for sum < 20 {
-		sum += sum
-		println(sum)
-	}
-
-	println()
-	// for com length de um Slice
-	nums := []int{10, 20, 30, 40, 50}
-	for i := 0; i < len(nums); i++ {
-		println(nums[i])
-	}
-
-	// //for infinito
-	// for {
-	// 	println("oi")
-	// }
-
-	println()
-	// com range, consigo ter a key e value
-	for key, value := range nums {
-		println(key, value)
-	}
+	cliente.Email = "teste@teste"
+	fmt.Println(cliente)
 }
